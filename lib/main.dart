b7659f38f7d1c056011468e813467c3749908d36
Simon Isinger <simon.isinger@web.de>
2025-04-04 13:02:24 +0200

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppAkademieCertificateView());
+}
+
+class AppAkademieCertificateView extends StatelessWidget {
+  const AppAkademieCertificateView({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+    );
+  }
+}
